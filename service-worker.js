008f0f96b522201d325e0e013a972a019c7adcbf
SiangWei91 <siangwei91@gmail.com>
2024-11-06 17:00:53 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -39,16 +39,4 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+
